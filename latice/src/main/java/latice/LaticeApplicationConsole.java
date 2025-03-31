c567768303ed5b59d3e7f9e75f59fd149ba8008a
Jules Didry <didryjules@gmail.com>
2025-03-31 19:04:07 +0200

    Commit "développé par Jules"

--- latice/src/main/java/latice/LaticeApplicationConsole.java
+++ latice/src/main/java/latice/LaticeApplicationConsole.java
@@ -4,6 +4,9 @@ public class LaticeApplicationConsole {
 	public static void main(String[] args) {
 		System.out.println("--------------------");
 		System.out.println("-- Bienvenue dans notre magnifique jeu de Latice --");
+		System.out.println("-- Développé par : Jules --");
 		System.out.println("--------------------");
+		
+		
 	}
 }
