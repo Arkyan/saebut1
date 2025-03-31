2d49f9758b32488b52a1e390dc85f19f0fe89881
Martin Valet <saussic0canard@gmail.com>
2025-03-31 19:08:52 +0200

    ajout nom martin
    titre.

--- latice/src/main/java/latice/LaticeApplicationConsole.java
+++ latice/src/main/java/latice/LaticeApplicationConsole.java
@@ -4,8 +4,9 @@ public class LaticeApplicationConsole {
 	public static void main(String[] args) {
 		System.out.println("--------------------");
 		System.out.println("-- Bienvenue dans notre magnifique jeu de Latice --");
-		System.out.println("-- Développé par : Jules --");
-		System.out.println("-- Développé par : Max --");
+		System.out.println("-- Développé par : Jules                         --");
+		System.out.println("-- Développé par : Max                           --");
+		System.out.println("-- Développé par : Martin                        --");
 		System.out.println("--------------------");
 		
 		
