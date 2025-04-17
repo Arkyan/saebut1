1c34e8a5e65dd563dc9d6cdbebf4f03be2149f82
DUCOURET Alexandre <duc6423alex@gmail.com>
2025-04-17 11:41:46 +0200

    add class Tile and add enumerations Color, Shapes -> completed

---
+++ latice/src/main/java/latice/model/slate/Tile.java
@@ -0,0 +1,84 @@
+package latice.model.slate;
+
+public class Tile {
+	private Color color;
+	private Shape shape;
+
+	public Tile(Color color, Shape shape) {
+			super();
+			this.color = color;
+			this.shape = shape;
+	}
+
+	public Color getColor() {
+		return color;
+	}
+
+	public Shape getShape() {
+		return shape;
+	}
+
+	public void describe() {
+		System.out.println("I am a " + color + " tile and my shape is " + shape.getSymbol());
+	}
+
+	public void showTile() {
+		String colorCode;
+		switch (color) {
+        	case Red:
+        		colorCode = "\u001B[31m";  // Red
+        		break;
+        	case Yellow:
+        		colorCode = "\u001B[33m";  // Yellow
+        		break;
+        	case Navy:
+        		colorCode = "\u001B[34m";  // Navy
+        		break;
+        	case Magenta:
+        		colorCode = "\u001B[35m";  // Magenta
+        		break;
+        	case Green:
+        		colorCode = "\u001B[32m";  // Green
+        		break;
+        	case Teal:
+        		colorCode = "\u001B[36m";  // Teal
+        		break;
+        	default:
+        		colorCode = "\u001B[0m";   // Reset color
+		}
+
+		String reset = "\u001B[0m";  // Reset console color
+		System.out.print(colorCode + shape.getSymbol() + reset);
+	}
+
+	public String getComboTile() {
+		String colorCode;
+		switch (color) {
+        	case Red:
+        		colorCode = "\u001B[31m";  // Red
+        		break;
+        	case Yellow:
+        		colorCode = "\u001B[33m";  // Yellow
+        		break;
+        	case Navy:
+        		colorCode = "\u001B[34m";  // Navy
+        		break;
+        	case Magenta:
+        		colorCode = "\u001B[35m";  // Magenta
+        		break;
+        	case Green:
+        		colorCode = "\u001B[32m";  // Green
+        		break;
+        	case Teal:
+        		colorCode = "\u001B[36m";  // Teal
+        		break;
+        	default:
+        		colorCode = "\u001B[0m";   // Reset color
+		}
+
+		String reset = "\u001B[0m";  // Reset console color
+    	return colorCode + shape.getSymbol() + reset;
+	}
+
+
+}
