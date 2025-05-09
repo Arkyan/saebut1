f5a33df512c95f79d2280691ff289812d783c28c
Martin Valet <saussic0canard@gmail.com>
2025-05-09 15:24:27 +0200

    Add controller for Board

--- latice/src/main/java/latice/controler/ihm/PrintBoardApp.java
+++ latice/src/main/java/latice/controler/ihm/PrintBoardApp.java
@@ -9,12 +9,11 @@ public class PrintBoardApp extends Application {
 
 	public static void main(String[] args) {
 		launch(args);
-
 	}
 
 	@Override
 	public void start(Stage primaryStage) throws Exception {
-		FXMLLoader loader = new FXMLLoader(getClass().getResource("src/main/java/latice/view/board-window.fxml"));
+		FXMLLoader loader = new FXMLLoader(getClass().getResource("../../view/board-window.fxml"));
 		Scene scene = new Scene(loader.load());
 		
 		primaryStage.setScene(scene);
