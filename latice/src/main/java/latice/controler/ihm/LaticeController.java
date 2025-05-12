a21e8018394f4d3a050fa31b6c39e670940c46a2
Max Gorce <max.gorce@yahoo.com>
2025-05-12 09:47:42 +0200

    undone fx launch

--- latice/src/main/java/latice/controler/ihm/LaticeController.java
+++ latice/src/main/java/latice/controler/ihm/LaticeController.java
@@ -1,14 +1,18 @@
 package latice.controler.ihm;
 
+import java.util.Optional;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.DialogPane;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.scene.control.TextInputDialog;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
-
-import java.util.Optional;
+import javafx.scene.layout.VBox;
+import latice.controler.Referee;
 
 public class LaticeController {
 
@@ -48,15 +52,24 @@ public class LaticeController {
 
     }
 
-    @FXML
+    
     String getNamePlayer(Integer nbPlayer) {
-        String namePlayer;
-        Label lblError;
+        String namePlayer = "";
+        Label lblError = new Label("Input a pseudo");
+        lblError.setVisible(false);
+        lblError.setStyle("-fx-text-fill: red; -fx-font-size: 12px; -fx-padding: 5px;");
         TextInputDialog playerNameInputDialog = new TextInputDialog("Pseudo");
 
-        playerNameInputDialog.setTitle("Input pseudo for player " + nbPlayer);
-        playerNameInputDialog.setHeaderText("Identification");
+        playerNameInputDialog.setTitle("Identification");
+        playerNameInputDialog.setHeaderText("Input pseudo for player " + nbPlayer);
         playerNameInputDialog.setContentText("Please enter your pseudo here: ");
+        
+        DialogPane dialogPane = playerNameInputDialog.getDialogPane();
+        TextField textField = (TextField) dialogPane.lookup(".text-field");
+        VBox content = new VBox(5);
+        content.getChildren().addAll(textField, lblError);
+        
+        dialogPane.setContent(content);
 
         Optional<String> pseudo = playerNameInputDialog.showAndWait();
         if (pseudo.isPresent()) {
@@ -64,9 +77,17 @@ public class LaticeController {
             return namePlayer;
         }
         else {
-
+			lblError.setVisible(true);
         }
-
-        return "";
+		return namePlayer;
+        
     }
+    
+    @FXML
+	void initialize() {
+		Referee referee = new Referee();
+		String namePlayer1 = getNamePlayer(1);
+		
+	}
+    
 }
